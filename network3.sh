252cb09f675f4643c3e0a87e416a18d37f3c9eac
dimXmanyu <130482773+dimXmanyu@users.noreply.github.com>
2024-10-10 14:38:14 +0800

    network3.sh

--- network3.sh
+++ network3.sh
@@ -4,8 +4,8 @@
 wget https://network3.io/ubuntu-node-v1.0.tar
 tar -xf ubuntu-node-v1.0.tar
 
-# 切换到 network3 目录并运行 manager.sh up
-cd network3
+# 切换到目录并运行 manager.sh up
+cd ubuntu-node
 sudo bash manager.sh up
 
 # 返回上级目录
@@ -15,11 +15,8 @@ cd ..
 wget https://network3.io/ubuntu-node-v1.1.tar
 tar -xf ubuntu-node-v1.1.tar
 
-# 切换到 network3 目录并运行 manager.sh key
-cd network3
+# 切换到目录并运行 manager.sh key
+cd ubuntu-node
 sudo bash manager.sh key
-
-# 给 manager.sh 赋予执行权限
-sudo chmod +x manager.sh
 
 echo "所有命令已成功执行！"
